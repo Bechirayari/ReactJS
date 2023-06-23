4f1a501aa291aff698cf8796c71ab46e54d44f91
Bechirayari <bechirayari240@gmail.com>
2023-06-23 11:54:12 +0100

    first commit react js checkpoint

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderTopp from './component/HeaderTopp';
+import Formulo from './component/Formulo';
+
+
+const App = () => {
+  return (
+    <div>
+
+      <HeaderTopp/>
+      <Formulo/>
+    </div>
+  )
+}
+
+export default App
---
+++ myapp/src/component/HeaderTopp.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import l1 from '../img/m1.jpg'
+import l2 from '../img/m2.jpg'
+import l3 from '../img/m3.jpg'
+const HeaderTopp = () => {
+  return (
+    <div>
+        
+        <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={l1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={l2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={l3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  )
+}
+
+export default HeaderTopp
